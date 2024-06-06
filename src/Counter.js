6f20f8916511101db2b0567cb9cf11d8b96102df
tusharkr16 <tusharkumar9871@gmail.com>
2024-06-06 22:16:07 +0530

    fixes bugs

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
-    const increement = () => {
+    const increment = () => {
         setCount((prevCount) => prevCount + 1);
-    }
-    const decreement = () => {
+    };
+    const decrement = () => {
         setCount((prevCount) => prevCount - 1);
-    }
+    };
     return (
         <div>
             <h1>Counter App</h1>
-            <p>Count : {count} </p>
-            <button onClick={increement}>Increement</button>
-            <button onClick={decreement}>Decreement</button>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
-    )
-}
+    );
+};
 
-export default Counter
+export default Counter;
